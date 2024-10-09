b382c04888fd24e8edd5c27ed34b1a5de9a5a488
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2024-10-09 14:50:15 +0530

    Update extra.py

--- codes/extra.py
+++ codes/extra.py
@@ -1,6 +1,10 @@
 import random
 import string
 
+def application_token_gen():
+    return ''.join([str(random.randint(0, 9)) for _ in range(15)])
+
+
 
 def generate_15_digit_alpha_token():
     characters = string.ascii_letters + string.digits  # Includes uppercase, lowercase, and digits
