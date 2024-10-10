82b03e29e03f8d894249f1a5db3c6b9d51e7d897
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2024-10-10 23:52:02 +0530

    Update Gateway.py

--- codes/Gateway.py
+++ codes/Gateway.py
@@ -13,21 +13,23 @@ from codes.extra import transaction_token
 USER_TOKEN = "994839198a4b6514dc9cc296c3bda19c"
 MONEY_NOTIFICATION = "rs added to your wallet successfully"
 
-async def createorder(applicationid , token , type):
+
+async def createorder(applicationid, token, type):
     try:
         gplink = users.find_one({"token": token},
-                                      {'mobile': 1, '_id': 0})
+                                {'mobile': 1, '_id': 0})
         if gplink is None:
             return JSONResponse(status_code=200,
                                 content={"success": False, "message": "No User Found"})
 
         transaction_code = f"{transaction_token()}"
         finddata = amount.find_one({"payment": "formcharge"},
-                                   {'loan1_5': 1, '_id': 0 , 'loan6_15': 1 , 'loan16_25': 1 , 'loan26_50': 1 , 'loan51_100': 1})
+                                   {'loan1_5': 1, '_id': 0, 'loan6_15': 1, 'loan16_25': 1, 'loan26_50': 1,
+                                    'loan51_100': 1})
         amt = forms.find_one({"application_no": applicationid},
-                                   {'loan_amount': 1, '_id': 0,})
+                             {'loan_amount': 1, '_id': 0, })
         global amounts
-        if type=="1":
+        if type == "1":
             if int(amt['loan_amount']) < 500000:
                 amounts = finddata['loan1_5']
             elif int(amt['loan_amount']) < 1500000:
@@ -38,21 +40,32 @@ async def createorder(applicationid , token , type):
                 amounts = finddata['loan26_50']
             elif int(amt['loan_amount']) < 10000000:
                 amounts = finddata['loan51_100']
+        elif type == "2":
+            finddata = amount.find_one({"payment": "file"},
+                                       {'agreement': 1, '_id': 0})
+            amounts = finddata['agreement']
+        elif type == "3":
+            finddata = amount.find_one({"payment": "file"},
+                                       {'insurance': 1, '_id': 0})
+            amounts = finddata['insurance']
 
             # amounts =  finddata['form_charge']
-        orderapi = await create_antipay(amounts , transaction_code , token)
+        orderapi = await create_antipay(amounts, transaction_code, token)
         if orderapi["status"] is True:
             transactiondb.insert_one(
                 {"token": token, "paid": False, "date": get_formatted_datetime(), "mobile": gplink['mobile'],
-                 "wallet_add": False, "amount":amounts, "transaction_id": transaction_code, "complete_date": "",
-                 "utr": "", "payment_url": orderapi['result']['payment_url'] , "application": applicationid})
+                 "wallet_add": False, "amount": amounts, "transaction_id": transaction_code, "complete_date": "",
+                 "utr": "", "payment_url": orderapi['result']['payment_url'], "application": applicationid,
+                 "type": type})
             link = await payment_qr(orderapi['result']['payment_url'])
             upic = await getdata(link)
             if upic is False:
                 return JSONResponse(status_code=200,
                                     content={"success": False, "message": "UPI Link Error"})
 
-            return JSONResponse(status_code=200, content={"success": True, "message": "Transaction Initiate" , "qr_code": link , "upi": upic , "orderid": transaction_code})
+            return JSONResponse(status_code=200,
+                                content={"success": True, "message": "Transaction Initiate", "qr_code": link,
+                                         "upi": upic, "orderid": transaction_code})
 
         else:
 
@@ -78,21 +91,32 @@ async def getstatus(transaction):
             return JSONResponse(status_code=200, content={"success": False, "message": "mismatch"})
         elif sta == "success":
 
-            gplink = transactiondb.find_one({"transaction_id": transaction},{'wallet_add': 1, 'paid': 1, '_id': 0 , 'amount': 1 , 'application': 1})
+            gplink = transactiondb.find_one({"transaction_id": transaction},
+                                            {'wallet_add': 1, 'paid': 1, '_id': 0, 'amount': 1, 'application': 1,
+                                             "type": 1})
 
             if gplink['wallet_add'] is False:
                 res = await getutr(transaction)
 
-                if res['status']  is True:
+                if res['status'] is True:
                     if res['result']['status'] == 'SUCCESS':
                         # data = forms.find_one({"token": gplink['token']},
                         #                   {'amount_paid': 1, '_id': 0})
 
                         transactiondb.update_one({"transaction_id": transaction},
-                                               {"$set": {"wallet_add": True , 'paid': True , 'utr':res['result']['utr'] ,  'complete_date': get_formatted_datetime() }})
+                                                 {"$set": {"wallet_add": True, 'paid': True,
+                                                           'utr': res['result']['utr'],
+                                                           'complete_date': get_formatted_datetime()}})
+                        if gplink['type'] == "1":
+                            forms.update_one({"application_no": gplink['application']},
+                                             {"$set": {"amount_paid": True}})
+                        elif gplink["type"] == "2":
+                            forms.update_one({"application_no": gplink['application']},
+                                             {"$set": {"agreement_amount": True}})
+                        elif gplink["type"] == "3":
+                            forms.update_one({"application_no": gplink['application']},
+                                             {"$set": {"insurance_amount": True}})
 
-                        forms.update_one({"application_no": gplink['application']},
-                                              {"$set": {"amount_paid": True}})
                         # await sendnotification(gplink['deviceid'] , f"{res['result']['amount']}{MONEY_NOTIFICATION}" )
                         # await updatetransactionforus(gplink['deviceid'] , "Add Money Through Anitoolz Gateway" ,gplink['amount'] , "0"  , str(int(data['wallet']) + res['result']['amount']) , await genratetransactionid() )
                         return JSONResponse(status_code=200, content={"success": True, "message": "Payment Paid"})
@@ -106,15 +130,17 @@ async def getstatus(transaction):
         print(e)
         return JSONResponse(status_code=200, content={"success": False, "message": "Something Went Wrong"})
 
+
 async def orderapi(orderid):
     url = "https://antitest.web24.site/api/check-order-status"
     datas = {
-    "user_token": USER_TOKEN,
-    "order_id": orderid
-}
+        "user_token": USER_TOKEN,
+        "order_id": orderid
+    }
 
-    res =  requests.post(url , datas)
+    res = requests.post(url, datas)
     return res.json()
+
 
 def generate_random_code():
     time_part = int(time.time() * 1000)
@@ -122,18 +148,21 @@ def generate_random_code():
     random_code = f"{time_part}{date_part}"
     return random_code
 
+
 async def getutr(transacion):
     url = 'https://antitest.web24.site/api/check-order-status'
-    data= {
-    "user_token": USER_TOKEN,
-    "order_id": transacion
-}
-    res = requests.post(url , data=data)
+    data = {
+        "user_token": USER_TOKEN,
+        "order_id": transacion
+    }
+    res = requests.post(url, data=data)
     return res.json()
 
+
 async def getBtyeid(orderid):
-    res =  requests.get(f"https://antitest.web24.site/get_byte_transaction_id.php?order_id={orderid}")
+    res = requests.get(f"https://antitest.web24.site/get_byte_transaction_id.php?order_id={orderid}")
     return res.json()
+
 
 async def getendstatus(byteid):
     try:
@@ -143,15 +172,15 @@ async def getendstatus(byteid):
             'byte_order_status': byteid
         }
 
-        res = requests.post(url , data=datas)
+        res = requests.post(url, data=datas)
 
-        return  res.text
+        return res.text
     except Exception as e:
         print(e)
         return "fail"
 
 
-async def create_antipay(wallet , transaction , token, ):
+async def create_antipay(wallet, transaction, token, ):
     datas = {
         "customer_mobile": token,
         "user_token": USER_TOKEN,
@@ -178,6 +207,7 @@ def get_formatted_datetime():
 
     return formatted_datetime
 
+
 async def payment_qr(url):
     new = url.replace("\\/", "/")
     response = requests.get(new)
@@ -199,6 +229,7 @@ async def payment_qr(url):
     else:
         return False
 
+
 def getdirect(url):
     parsed_url = urlparse(url)
     query_params = parse_qs(parsed_url.query)
@@ -206,6 +237,7 @@ def getdirect(url):
     # Get the 'data' parameter value
     data_value = query_params.get('data', [None])[0]
     return quote(data_value)
+
 
 async def getdata(url):
     parsed_url = urlparse(url)
@@ -219,4 +251,3 @@ async def getdata(url):
         return decoded_data
     else:
         return False
-
