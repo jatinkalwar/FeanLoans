9f9ccf1ab106524371de282b3cd254dca306bfed
Jatin Kalwar <77274095+jatinkalwar@users.noreply.github.com>
2024-10-17 12:01:40 +0530

    Update Models.py

--- codes/Models.py
+++ codes/Models.py
@@ -6,21 +6,21 @@ from typing import Optional
 
 from starlette.middleware.cors import CORSMiddleware
 
-
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None, title="AntiToolz")
 app.add_middleware(
+
     CORSMiddleware,
     allow_origins=["*"],  # Allows all origins. Change this to specific origins for more security.
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
     allow_headers=["*"],  # Allows all headers
 )
-client = MongoClient("mongodb+srv://jatinkalwar:shifaanam@mbomb.ghtntua.mongodb.net")
-users = client.msdigital.users
-otp_coll = client.msdigital.otp
-forms = client.msdigital.forms
-amount = client.msdigital.payment
-transactiondb = client.msdigital.transaction
+client = MongoClient("mongodb+srv://feanloans:*****@feanloans.tw0rt.mongodb.net")
+users = client.feans.users
+otp_coll = client.feans.otp
+forms = client.feans.forms
+amount = client.feans.payment
+transactiondb = client.feans.transaction
 
 
 
